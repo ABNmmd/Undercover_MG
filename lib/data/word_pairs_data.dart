0ba9c939005d115f87d560743a035e6039a2fcc5
ABNmmd <moha.ww.ha@gmail.com>
2025-06-28 01:12:08 +0100

    Add WordPairData class with predefined citizen and undercover word pairs

---
+++ lib/data/word_pairs_data.dart
@@ -0,0 +1,26 @@
+import '../models/word_pair.dart';
+
+class WordPairData {
+  static const List<WordPair> wordPairs = [
+    WordPair(citizenWord: 'Cat', undercoverWord: 'Tiger'),
+    WordPair(citizenWord: 'Coffee', undercoverWord: 'Tea'),
+    WordPair(citizenWord: 'Ship', undercoverWord: 'Boat'),
+    WordPair(citizenWord: 'Car', undercoverWord: 'Bus'),
+    WordPair(citizenWord: 'Apple', undercoverWord: 'Orange'),
+    WordPair(citizenWord: 'Book', undercoverWord: 'Magazine'),
+    WordPair(citizenWord: 'Rain', undercoverWord: 'Snow'),
+    WordPair(citizenWord: 'Beach', undercoverWord: 'Desert'),
+    WordPair(citizenWord: 'Pizza', undercoverWord: 'Burger'),
+    WordPair(citizenWord: 'Guitar', undercoverWord: 'Piano'),
+    WordPair(citizenWord: 'Summer', undercoverWord: 'Winter'),
+    WordPair(citizenWord: 'Mountain', undercoverWord: 'Valley'),
+    WordPair(citizenWord: 'Doctor', undercoverWord: 'Nurse'),
+    WordPair(citizenWord: 'Basketball', undercoverWord: 'Football'),
+    WordPair(citizenWord: 'Chocolate', undercoverWord: 'Vanilla'),
+    WordPair(citizenWord: 'Moon', undercoverWord: 'Sun'),
+    WordPair(citizenWord: 'River', undercoverWord: 'Lake'),
+    WordPair(citizenWord: 'Spider', undercoverWord: 'Ant'),
+    WordPair(citizenWord: 'Hotel', undercoverWord: 'Motel'),
+    WordPair(citizenWord: 'Knife', undercoverWord: 'Fork'),
+  ];
+}
